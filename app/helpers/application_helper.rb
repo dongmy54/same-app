a63f136d18a74c65f1c701a98a3262b8d5a4322c
dongmy54 <615760433@qq.com>
2017-07-28 09:49:58 +0800

    add full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def full_title(active_title="")
+    base_title = "Ruby On Rails"
+
+    if active_title.empty?
+      base_title
+    else
+      active_title + " | " + base_title
+    end 
+  end
+  
 end
