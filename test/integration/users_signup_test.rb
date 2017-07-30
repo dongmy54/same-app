2bcdb95738af3acde9d7a1d958edfbde5c40dbf5
dongmy54 <615760433@qq.com>
2017-07-30 21:59:53 +0800

    let sginup url same after sign up failures

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -21,7 +21,7 @@ class UserSignupTest < ActionDispatch::IntegrationTest
     get new_user_path
 
     assert_difference 'User.count', 1 do
-      post users_path, params: { user:{ name: "Example User",
+      post signup_path, params: { user:{ name: "Example User",
                                         email: "user@example.com",
                                         password: "password",
                                         password_confirmation: "password"}}
