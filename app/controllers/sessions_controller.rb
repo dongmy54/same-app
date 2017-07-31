7bd89a998f83f0330ceabecc5cade9977a3040fb
dongmy54 <615760433@qq.com>
2017-07-31 17:06:19 +0800

    finish login feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.find_by(email: params[:session][:email].downcase)
+    
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      redirect_to @user
+    else
+      flash.now[:warning] = "邮箱或者密码无效！" 
+      render 'new'
+    end 
+  end
+
 end
